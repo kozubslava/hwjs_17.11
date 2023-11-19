f049da55b0a9b2292cd9c94eaeec59d7a783372b
Вячеслав <146669463+kozubslava@users.noreply.github.com>
2023-11-19 16:38:12 +0200

    add home work function

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,11 @@
-// 1
+// 1) Знайти суму чисел у диапазоні від 1 до 100.
 let sum = 0
 for (let i = 1; i <= 100; i++) {
   sum += i
-
-  console.log(sum);
 }
+console.log(sum);
 
- // 2
+// 2) Пропонувати користувачу розв'язувати рівняння (2 + 2 * 2) до тих пір, поки користувач не надасть правильну відповідь
 let userResult;
 const correctResult = 6;
 while (userResult !== correctResult) {
@@ -17,39 +16,18 @@ while (userResult !== correctResult) {
   }
 }
 
+// 3) Створити функцію, яка буде приймати два числа, та буде повертати значення першого числа у ступені другого. Заборонено використовувати оператор ** та Math.pow();. Ступінь може бути позитивною або нульовою
 
-
-
-// let userAnswer;
-// const correctAnswer = 2 + 2 * 2;
-
-// do {
-//     userAnswer = parseInt(prompt("Розв'яжіть рівняння: (2 + 2 * 2)"));
-// } while (userAnswer !== correctAnswer);
-
-// console.log("Правильно!");
-
-
-
-// let userAnswer;
-// const correctAnswer = 2 + 2 * 2;
-
-// while (true) {
-//     userAnswer = prompt("Розв'яжіть рівняння: (2 + 2 * 2)");
-
-//     if (userAnswer === correctAnswer) {
-//         console.log("Правильно!");
-//         break;
-//     } else {
-//         console.log("Спробуйте ще раз.");
-//     }
-// }
-
-
-
-
-
-
+const calc = function (a, b) {
+  let result = 1;
+  for (let i = 1; i <= b; i++) {
+    result *= a
+    
+  }
+  return(result);
+} 
+  
+console.log (calc(3, 4));
 
 
 
